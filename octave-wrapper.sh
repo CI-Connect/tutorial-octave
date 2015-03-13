bd48e767bbf0a140e50cfee7e442df28dcc3471b
bala <dmbala@gmail.com>
2015-03-13 15:34:45 -0500

    add module load libgfortran and remove source line in the wrapper script

--- octave-wrapper.sh
+++ octave-wrapper.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
- 
-EXPECTED_ARGS=1
- 
-if [ $# -ne $EXPECTED_ARGS ]; then
-  echo "Usage: octave-wrapper.sh file.octave"
-  exit 1
-else
-  source /cvmfs/oasis.opensciencegrid.org/osg/palms/sw/octave/el5/x86_64/default/setup.sh
-  octave $1
-fi
+  module load fftw/3.3.4 atlas lapack hdf5 qhull pcre SparseSuite glpk octave libgfortran
+  module load octave 
+  octave $1 
